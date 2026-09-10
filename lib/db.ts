fd51ceb25ab653fc6b94d1ec662ed4560de47b10
LokeshAshapu <lokeshashapu@gmail.com>
2026-09-10 20:55:29 +0530

    Update database name to storage.db

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
 if (!process.env.DATABASE_URL) {
-  process.env.DATABASE_URL = 'file:./dev.db'
+  process.env.DATABASE_URL = 'file:./storage.db'
 }
 
 const globalForPrisma = globalThis as unknown as {
